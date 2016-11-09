44edd7faecd08a2ed4eebf673339413f50f96780
MaelQuemard <mael.quemard@hotmail.fr>
2016-11-09 16:12:29 +0100

    Add questions/score

--- charliesQuizz/assets/js/app.js
+++ charliesQuizz/assets/js/app.js
@@ -31,103 +31,116 @@ app.run(['GApi', 'GAuth',
 
 
 app.controller('myController', ['$scope', '$location', 'GApi', function($scope, $location, GApi){
-	$scope.play = function(){
-		// Ici recuperation des questions à partir du endpoint
-		// Probleme endpoint et methode
-		GApi.execute('scoreEntityEndPoint', 'listQuestions').then( function(resp) {
-			console.log("here");
-			console.log(resp);
-			$scope.sparqlResult = resp.items;
-		  }, function() {
-			console.log("error :(");
-		});
-	  	$scope.nextQuestion();
-	};
+		$scope.play = function(){
 
-
-	$scope.category = function(){
-	  $location.path('/category');
-	  
-	  // En attendant que le endpoint soit OK :
-	  $scope.category = { 
-		categ: ["Scientist",
-		"Monument",
-		"Autres"]
-	  };
-
-	  // Ici je recupere les differentes categories
-	  // GApi.execute('scoreEntityEndPoint', 'listCategory').then( function(resp) {
-	  //       $scope.category = resp.items;
-	  //     }, function() {
-	  //       console.log("error :(");
-	  //   });
-	};
-
-	// Question suivantes 
-	/* (selon le nb de question passé et 
-		a quelle section du resultat sparql on en est !)
-	*/
-	$scope.nextQuestion = function() {
-		$location.path('/play');
-
-		if ($scope.nbQuestion == 2){
-			$scope.nbQuestion = 0;
-			$scope.nbSection++;
-		}
-		 else{
-			$scope.nbQuestion++;
-			// $scope.questionAnswer = {
-			//   $scope.question: $scope.sparqlResult,
-			//   $scope.answers: [
-			//     "Do"
-			//   ]
-			// }
-		}
-
-		$scope.questions = {
-			question: "Here you find the question..",
-			answers: [
-				"First",
-				"Second",
-				"Third"
-			],
-			good_answer: Math.floor(Math.random()*3),
-			answered: false
+			// Ici recuperation des questions à partir du endpoint
+			GApi.execute('charlies', 'charliesEndpoint.listQuestions',{number: 10, category:"scientist"}).then( function(resp) {
+				$scope.sparqlResult = resp.items;
+				$scope.nextQuestion();
+			  }, function() {
+				console.log("error :(");
+			});
 		};
-	};
 
-	// Validé la question (On a decider de mettre la premiere reponse bonne mais pas dans la vue)
-	// A revoir
-	$scope.valid = function(answer){
-		$scope.questions.answered = answer;
-		if ($scope.questions.good_answer == $scope.questions.answered) {
-			$scope.myscore += 10;
+
+		$scope.category = function(){
+		  $location.path('/category');
+		  
+		  // En attendant que le endpoint soit OK :
+		  $scope.category = { 
+			categ: ["Scientist",
+			"Monument",
+			"Autres"]
+		  };
+
+		  // Ici je recupere les differentes categories
+		  // GApi.execute('scoreEntityEndPoint', 'listCategory').then( function(resp) {
+		  //       $scope.category = resp.items;
+		  //     }, function() {
+		  //       console.log("error :(");
+		  //   });
 		};
-		$scope.nbQuestion++;
-	};
 
-	$scope.highscore = function () {
-		$location.path('/highscore');
+		// Question suivantes 
+		/* (selon le nb de question passé et 
+			a quelle section du resultat sparql on en est !)
+		*/
+		$scope.nextQuestion = function() {
+			$location.path('/play');
 
-		// Recuperation des highscores
-		GApi.execute('scoreentityendpoint', 'listScoreEntity').then( function(resp) {
-			console.log(resp);
-			$scope.high = resp.items;
-		}, function() {
-			console.log("error :(");
-		});
-	}
+			console.log($scope.sparqlResult);
 
-	// Variable qui gere les pages
-	$scope.page = 'Accueil';
-	// Variable pour recuperer les resultats sparql
-	$scope.sparqlResult;
-	// nbQuestion courant
-	$scope.nbQuestion = 0;
-	// nbSection courant
-	$scope.nbSection = 0;
-	// score (a voir si on fait ca comme ca !)
-	$scope.myscore = 0;
+			$scope.questions = $scope.sparqlResult[$scope.cursorSection][$scope.cursorQuestion];
+
+/*			$scope.questions = {
+				question: "who ?",
+				answers: [
+					"good",
+					"bad",
+					"bad2",
+					"bad3"
+				]
+			};*/
+			$scope.questions.pic = $scope.sparqlResult[$scope.cursorSection].pic;
+			$scope.questions.answered = false;
+			$scope.good_answer = $scope.questions.answers[0];
+
+			$.randomize($scope.questions.answers);
+
+			for (var i = $scope.questions.answers.length - 1; i >= 0; i--) {
+				if($scope.questions.answers[i] == $scope.good_answer){
+					$scope.questions.good_answer = i;
+				}
+			};
+
+			switch($scope.cursorQuestion) {
+				case "who":
+					$scope.cursorQuestion = "when";
+					break;
+				case "when":
+					$scope.cursorQuestion = "where";
+					break;
+				case "where":
+					$scope.cursorQuestion = "who";
+					$scope.cursorSection++;
+					break;
+			}
+		};
+
+		// Validé la question (On a decider de mettre la premiere reponse bonne mais pas dans la vue)
+		// A revoir
+		$scope.valid = function(answer){
+			console.log(answer);
+			$scope.questions.answered = answer;
+			if ($scope.questions.good_answer == answer) {
+				$scope.myscore += 10;
+			};
+			console.log($scope.myscore);
+			//$scope.nextQuestion();
+		};
+
+		$scope.highscore = function () {
+			$location.path('/highscore');
+
+			// Recuperation des highscores
+			GApi.execute('charlies', 'charliesEndpoint.listHighscores', {category: "scientist"}).then( function(resp) {
+				console.log(resp);
+				$scope.high = resp.items;
+			}, function() {
+				console.log("error :(");
+			});
+		}
+
+		// Variable qui gere les pages
+		$scope.page = 'Accueil';
+		// Variable pour recuperer les resultats sparql
+		$scope.sparqlResult = null;
+		// nbQuestion courant
+		$scope.cursorSection = 0;
+		//
+		$scope.cursorQuestion = "who";
+		// score (a voir si on fait ca comme ca !)
+		$scope.myscore = 0;
 	}
 ]);
 
@@ -140,3 +153,10 @@ function onSignIn(googleUser) {
   console.log('Image URL: ' + profile.getImageUrl());
   console.log('Email: ' + profile.getEmail());
 }
+
+(function($) {
+	$.randomize = function(arr){
+		for (var j, x, i = arr.length; i; j = parseInt(Math.random()*i), x = arr[--i], arr[i] = arr[j], arr[j] = x);
+		return arr;
+	};
+})(jQuery);
